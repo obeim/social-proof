03817d9a41924838df4f1c713de062ffebde536f
obei <obei.m2017@gmail.com>
2021-01-15 20:46:48 +0200

    social proof

--- index.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const app = express();
-app.use(express.static("public"));
-app.set("view engine", "ejs");
-app.get("/", (req, res) => {
-  res.render("index");
-});
-
-app.listen(process.env.PORT || 3000);
